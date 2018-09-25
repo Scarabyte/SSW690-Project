782c728aec4f8cdc1b6ba134bd306171dae7100a
Scarabyte <Scarabyte@users.noreply.github.com>
2018-09-25 16:22:32 -0400

    Remove commented code
    Research suggests that 'HoughLinesP' would be a better choice for
    this application rather than 'HoughLines', but if we do want to
    change algorithms later, we can retrieve the code from this or
    and earlier commit.

--- src/opencv_proj/app/src/main/java/com/example/opencvproject/LaneDetector.java
+++ src/opencv_proj/app/src/main/java/com/example/opencvproject/LaneDetector.java
@@ -43,19 +43,6 @@ public class LaneDetector {
             pt2.y = val[3];
             Imgproc.line(outputImage, pt1, pt2, new Scalar(0, 0, 255), 10);
         }
-//        Imgproc.HoughLines(outputImage, linesHough, 1, 2*Math.PI/180, 20);
-//        for (int i = 0; i < linesHough.cols(); i++){
-//            double data[] = linesHough.get(0, i);
-//            double rho = data[0];
-//            double theta = data[1];
-//            double cosTheta = Math.cos(theta);
-//            double sinTheta = Math.sin(theta);
-//            double x0 = cosTheta * rho;
-//            double y0 = sinTheta * rho;
-//            Point pt1 = new Point(x0 + 10000 * (-sinTheta), y0 + 10000 * cosTheta);
-//            Point pt2 = new Point(x0 - 10000 * (-sinTheta), y0 - 10000 * cosTheta);
-//            Imgproc.line(outputImage, pt1, pt2, new Scalar(0, 0, 200), 3);
-//        }
 
         return lanePoints;
     }
