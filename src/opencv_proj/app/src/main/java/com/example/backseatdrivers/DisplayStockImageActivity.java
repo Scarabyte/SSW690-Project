7cb40492f83f303c1ee226eae3682ca8fc50703a
Keith Roseberry <kroseber@stevens.edu>
2018-10-14 10:19:49 -0400

    Successfully displayed the stock image in a new Activity.

--- src/opencv_proj/app/src/main/java/com/example/backseatdrivers/DisplayStockImageActivity.java
+++ src/opencv_proj/app/src/main/java/com/example/backseatdrivers/DisplayStockImageActivity.java
@@ -2,16 +2,27 @@ package com.example.backseatdrivers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class DisplayStockImageActivity extends AppCompatActivity {
+
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_stock_image);
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("It's ALIVE!!!");
+        mLinearLayout = new LinearLayout(this);
+        ImageView i = new ImageView(this);
+        i.setImageResource(R.drawable.single_lane_3);
+        i.setAdjustViewBounds(true);
+        i.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT)));
+        mLinearLayout.addView(i);
+        setContentView(mLinearLayout);
     }
 }
